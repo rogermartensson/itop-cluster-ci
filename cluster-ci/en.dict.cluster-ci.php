ed818622e3b52d101921d514616580417d8c4039
Roger Mårtensson <roger.martensson@gmail.com>
2019-04-11 19:41:16 +0200

    Finish balancer

--- cluster-ci/en.dict.cluster-ci.php
+++ cluster-ci/en.dict.cluster-ci.php
@@ -50,11 +50,22 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:WebApplication/Attribute:dbschema_list+' => '',
     'Class:WebApplication/Attribute:softwareinstance_id' => 'Web Server',
     'Class:WebApplication/Attribute:softwareinstance_id+' => '',
+    'Class:WebApplication/Attribute:lbaddress_id' => 'LB Address',
+    'Class:WebApplication/Attribute:lbaddress_id+' => '',
 
 	'Class:LoadBalancer' => 'Load Balancer',
 	'Class:LoadBalancer+' => '',
 	'Class:LoadBalancer/Attribute:lbnodes_list' => 'LB Nodes',
 	'Class:LoadBalancer/Attribute:lbnodes_list+' => '',
+	'Class:LoadBalancer/Attribute:lbaddresses_list' => 'LB Addresses',
+	'Class:LoadBalancer/Attribute:lbaddresses_list+' => '',
+
+	'Class:LBAddress' => 'LB Address',
+	'Class:LBAddress+' => '',
+	'Class:LBAddress/Attribute:loadbalancer_id' => 'Load Balancer',
+	'Class:LBAddress/Attribute:loadbalancer_id+' => '',
+	'Class:LBAddress/Attribute:webapp_list' => 'Web Applications',
+	'Class:LBAddress/Attribute:webapp_list+' => '',
 
 	'Class:lnkDBServerToDBCluster' => 'Link DB Server / DB Cluster',
 	'Class:lnkDBServerToDBCluster+' => '',
